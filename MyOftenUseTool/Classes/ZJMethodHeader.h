333f7fdae1c5a49fc9ee6b54d1373e5e4b043603
zhangjiang <896884553@q.com>
2016-12-29 09:39:52 +0800

    添加屏幕的宽度和高度的宏定义

--- MyOftenUseTool/Classes/ZJMethodHeader.h
+++ MyOftenUseTool/Classes/ZJMethodHeader.h
@@ -37,6 +37,16 @@
 #define KAppVersion [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"]
 
 /**
+ 屏幕宽度
+ */
+#define KScreenWidth [UIScreen mainScreen].bounds.size.width
+
+/**
+ 屏幕高度
+ */
+#define KScreenHeight [UIScreen mainScreen].bounds.size.height
+
+/**
  生成颜色值
  */
 #define KRGB(r,g,b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1]
