787bc78a58c5aa2b68cca221034512f8a03404a3
zhangjiang <896884553@q.com>
2016-12-30 16:54:02 +0800

    修改了版本号，修改了出错的宏定义

--- MyOftenUseTool/Classes/ZJMethodHeader.h
+++ MyOftenUseTool/Classes/ZJMethodHeader.h
@@ -95,7 +95,7 @@
  打印输出
 */
 #if DEBUG
-#define NSLog(FORMAT, ...) fprintf(stderr,"\nfunction:%s line:%d content:%s\n", __FUNCTION__, __LINE__, ［NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
+#define NSLog(FORMAT, ...) fprintf(stderr,"\nfunction:%s line:%d content:%s\n", __FUNCTION__, __LINE__, [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
 #else
 #define NSLog(FORMAT, ...) nil
 #endif
